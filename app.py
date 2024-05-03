a57ca44b6abf6b71cd0452cac004b3b325cde2db
Mahamat Saleh <mahamatsaleh6261@gmail.com>
2024-05-03 13:06:22 +0000

    update app

--- app.py
+++ app.py
@@ -19,11 +19,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
-plt.plot(x, y)
+fig, ax = plt.subplots()
+ax.plot(x, y)
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Graphique de sin(x)')
-st.pyplot()
+
+st.pyplot(fig)
+
+
 
 # Changez le thème
 st.markdown("""
